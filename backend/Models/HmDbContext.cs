ec742af347e452f3329e69a4977dd902b5461183
mihael.golob <mihael.golob@outfit7.com>
2025-11-02 22:54:12 +0100

    add .net identity to the project which will be used for user authentication

---
+++ backend/Models/HmDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models;
+
+public class HmDbContext(DbContextOptions<HmDbContext> options) : IdentityDbContext(options) {
+    protected override void OnModelCreating(ModelBuilder builder) {
+        base.OnModelCreating(builder);
+
+        // todo add habit models
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,17 +1,27 @@
+using backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<HmDbContext>();
+builder.Services.AddDbContext<HmDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Debug")));
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) {
-    app.MapOpenApi();
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<IdentityUser>();
 app.Run();
